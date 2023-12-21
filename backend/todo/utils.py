51068a109c607787c91b026841335049b5abee76
Md Imran Hosen <62979868+MdImranHosen1@users.noreply.github.com>
2023-12-22 02:14:09 +0600

    Add edit backend-frontend

---
+++ backend/todo/utils.py
@@ -0,0 +1,43 @@
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+
+def getTodosList(request):
+    todos=Todo.objects.all()
+    serializer=TodoSerializer(todos,many=True)
+    return Response(serializer.data)
+
+def getTodoDetail(request,pk):
+    todos=Todo.objects.get(id=pk)
+    serializer=TodoSerializer(todos,many=False)
+    return Response(serializer.data)
+
+
+def updateTodo(request, pk):
+    data = request.data
+    todo = Todo.objects.get(id=pk)
+    serializer = TodoSerializer(instance=todo, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return serializer.data
+
+
+def createTodo(request):
+    data=request.data
+    todo=Todo.objects.create(
+        title=data['title'],
+        description=data['description']    
+    )
+    serializer=TodoSerializer(instance=todo,data=data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    
+    return serializer.data
+
+def deleteTodo(request,pk):
+    todo= Todo.objects.get(id=pk)
+    todo.delete()
+    return Response("Task was deleted")
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer
+from rest_framework.decorators import api_view
 from .models import Todo
-
+from .utils import updateTodo, getTodoDetail, deleteTodo, getTodosList, createTodo
 # Create your views here.
 
 class TodoView(viewsets.ModelViewSet):
     serializer_class=TodoSerializer
     queryset=Todo.objects.all()
+
+
+# /getTodo GET
+# /getTodo POST
+# /getTodo/<id> GET
+# /getTodo/<id> PUT
+# /getTodo/<id> DELETE
+
+@api_view(['GET', 'POST'])
+def getTodos(request):
+
+    if request.method == 'GET':
+        return getTodosList(request)
+
+    if request.method == 'POST':
+        return createTodo(request)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def getTodo(request, pk):
+
+    if request.method == 'GET':
+        return getTodoDetail(request, pk)
+
+    if request.method == 'PUT':
+        return updateTodo(request, pk)
+
+    if request.method == 'DELETE':
+        return deleteTodo(request, pk)
